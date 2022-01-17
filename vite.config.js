77f3a5cfd63c86a2eac16456d2151210dffdd88e
bryzZz <tebloev.nikita@mail.ru>
2022-01-17 19:13:38 +0800

    change configs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: process.env.VITE_PORT || 3000,
-    },
+    base: '/text-analytics-Azure/',
 });
